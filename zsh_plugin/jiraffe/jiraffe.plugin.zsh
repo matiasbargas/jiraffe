9b402c31bf3ed71e45d19cc86f6b74137852df72
Miguel Oliva <moliva@mulesoft.com>
2014-05-21 20:38:00 -0300

    Updated zsh plugin with components and show commands

--- zsh_plugin/jiraffe/jiraffe.plugin.zsh
+++ zsh_plugin/jiraffe/jiraffe.plugin.zsh
@@ -2,7 +2,7 @@
 
 # completion function
 _jiraffe () {
-    local -a _1st_arguments _create_dopts _common_dopts
+    local -a _1st_arguments _create_dopts _common_dopts _show_dopts _components_dopts
     local expl
     typeset -A opt_args
 
@@ -12,23 +12,32 @@ _jiraffe () {
 	'(-p)-p[Project ID]:project key:(STUDIO MULE AUTOMATION EE APIKIT ION SIT)'
     )
 
+    _components_dopts=(
+    	'*:project key:(STUDIO MULE AUTOMATION EE APIKIT ION SIT)'
+    )
+
+   _show_dopts=(
+    	'*:issue key:'
+    )
+
     _create_dopts=(
     	'(-r)-r[Reporter]:jira id:($JIRA_ID)'
 	'(-a)-a[Assignee]:jira id:($JIRA_ID)'
 	'(-pr)-pr[Issue priority]:priority:(Major Critical Blocker Trivial Minor)'
+	'(-c)-c[Component name]:component name:(Canvas DataMapper ApiKit Automation Batch Build Classpath "Cloud Connectors" Commons Components DataSense Debugger "Document Generation" Documentation "Drag and Drop" "Embedded Server" Examples Executino "Import / Export" Installation Maven "MEL Autocompletion" Metadata Plugin "Project Structure" Release SCM Tests "Update Site" Usability "User Interface" Validations "XML Auto Completion" "XML Generation")'
     )
 
     _1st_arguments=(
+    'components:Display components for a given project' \
     'create:Create a jira issue and return the issue ID' \
-    'show:Show information for a given issue id'\
-    'components:Show components for a given project id' \
     'default:Show current default values' \
+    'show:Display issue information' \
     'update:Update current default values' \
     'help:Display help' \
     )
      _arguments \
-    '*:: :->subcmds' && return 0
-
+    '*:: :->subcmds' && return 0 
+     
     if (( CURRENT == 1 )); then
         _describe -t commands "jiraffe subcommand" _1st_arguments
         return
@@ -44,6 +53,14 @@ _jiraffe () {
 	_arguments \
 	$_common_dopts \
         ;;
+	show)
+	_arguments \
+	$_show_dopts \
+	;;
+	components)
+	_arguments \
+	$_components_dopts \
+	;;
     esac
 
 }
